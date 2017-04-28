c877c3c41496441e1344d7a5b1c4b4f5fded0c78
Ziga Besal <besal.ziga@gmail.com>
2017-04-28 15:34:38 +0200

    Favicon, compressed images

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function hoverProfileImage(element) {
+    element.setAttribute("src", "img/icon.jpg");
+}
+function unhoverProfileImage(element) {
+    element.setAttribute("src", "img/profile-image.jpg");
+}
