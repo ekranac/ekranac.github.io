01fc7bbd27b572809733f624ba6d7d6e9d199a58
Ziga Besal <besal.ziga@gmail.com>
2017-07-01 13:39:27 +0200

    Content update

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ function hoverProfileImage(element) {
     element.setAttribute("src", "img/icon.jpg");
 }
 function unhoverProfileImage(element) {
-    element.setAttribute("src", "img/profile-image.jpg");
+    element.setAttribute("src", "img/profile-image.png");
 }
